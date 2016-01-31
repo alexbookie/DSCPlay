8ce5f507b76864ea3f0abd7d53abee4e9b0c3fee
alexbookie <alex.bookless@waterstons.com>
2016-01-31 13:10:06 +0000

    Tweaked setuplab for securer creds and autoreboot

--- SetupLab.ps1
+++ SetupLab.ps1
@@ -1,15 +1,25 @@
-$secpasswd = ConvertTo-SecureString "NotReallySecure1" -AsPlainText -Force
-$domainCred = New-Object System.Management.Automation.PSCredential ("psdsc\Administrator", $secpasswd)
-$safemodeAdministratorCred = New-Object System.Management.Automation.PSCredential ("psdsc\Administrator", $secpasswd)
-$newpasswd = ConvertTo-SecureString "StillNotSecure1" -AsPlainText -Force
-$userCred = New-Object System.Management.Automation.PSCredential ("psdsc\dummy", $newpasswd)
-
 configuration PSDSCLab
 {
+    param             
+    (             
+        [Parameter(Mandatory)]             
+        [pscredential]$safemodeAdministratorCred,             
+        [Parameter(Mandatory)]            
+        [pscredential]$domainCred,
+        [Parameter(Mandatory)]            
+        [pscredential]$userCred              
+    )         
     Import-DscResource -ModuleName xActiveDirectory
 
     Node $AllNodes.Where{$_.Role -eq "DC"}.Nodename
     {
+        LocalConfigurationManager            
+        {            
+            ActionAfterReboot = 'ContinueConfiguration'            
+            ConfigurationMode = 'ApplyOnly'            
+            RebootNodeIfNeeded = $true            
+        }       
+        
         WindowsFeature ADDSInstall
         {
             Ensure = "Present"
@@ -54,7 +64,19 @@ configuration PSDSCLab
     }
 }
 
+
+
 $config = Invoke-Expression (Get-content $PSScriptRoot\LabConfig.psd1 -Raw)
-PSDSCLab -configurationData $config
+PSDSCLab -configurationData $config `
+    -safemodeAdministratorCred (Get-Credential -UserName '(Password Only)' `
+        -Message "New Domain Safe Mode Administrator Password") `
+    -domainCred (Get-Credential -UserName psdsc\alex `
+        -Message "New Domain Admin Credential") `
+    -userCred (Get-Credential -UserName psdsc\dummy `
+        -Message "New Dummy User Credential") `
+      
+
+# Make sure that LCM is set to continue configuration after reboot            
+Set-DSCLocalConfigurationManager -Path .\PSDSCLab –Verbose   
 
 Start-DscConfiguration -ComputerName ps-dc-01 -Wait -Force -Verbose -path .\PSDSCLab -Debug
